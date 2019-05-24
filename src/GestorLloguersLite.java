c756b3f74dfe16373b4ad13e2acc7d099e56fb1e
Isvy Araujo <ikelisonva.jmzafra@gmail.com>
2019-05-24 19:35:18 +0200

    Exercici 6. Preparant el terreny: ara seriosament, proves unitàries : Completat, proves creades en GestorLloguersLite a baix de tot.

--- src/GestorLloguersLite.java
+++ src/GestorLloguersLite.java
@@ -40,7 +40,13 @@ public class GestorLloguersLite {
     }
 
     @Test
-    public void testInforme() throws ParseException {
+    public void testInforme0(){
+        Client isvy = new Client("Y1846344X", "Isvy Kelison", "665239406");
+        System.out.println(isvy.informe());
+    }
+
+    @Test
+    public void testInforme1BASIC() throws ParseException {
         Vehicle vehicleBasic = new Vehicle("Corsa", "Opel", Vehicle.BASIC);
         SimpleDateFormat dateFormat = new SimpleDateFormat("d/M/yyyy");
         Date date = dateFormat.parse("2/8/2013");
@@ -49,4 +55,79 @@ public class GestorLloguersLite {
         isvy.afegeix(lloguerBasic);
         System.out.println(isvy.informe());
     }
+    @Test
+    public void testInforme2BASIC() throws ParseException {
+        Vehicle vehicleBasic = new Vehicle("Corsa", "Opel", Vehicle.BASIC);
+        Vehicle vehicleBasic2 = new Vehicle("Seat", "Ibiza", Vehicle.BASIC);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("d/M/yyyy");
+        Date date = dateFormat.parse("2/8/2013");
+        Date date2 = dateFormat.parse("9/8/2013");
+        Lloguer lloguerBasic = new Lloguer(date, 2, vehicleBasic);
+        Lloguer lloguerBasic2 = new Lloguer(date2, 3, vehicleBasic2);
+        Client isvy = new Client("Y1846344X", "Isvy Kelison", "665239406");
+        isvy.afegeix(lloguerBasic);
+        isvy.afegeix(lloguerBasic2);
+        System.out.println(isvy.informe());
+    }
+    @Test
+    public void testInforme1GENERAL() throws ParseException {
+        Vehicle vehicleGeneral = new Vehicle("CL6", "Mercedez", Vehicle.GENERAL);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("d/M/yyyy");
+        Date date = dateFormat.parse("2/8/2013");
+        Lloguer lloguerGeneral = new Lloguer(date, 2, vehicleGeneral);
+        Client isvy = new Client("Y1846344X", "Isvy Kelison", "665239406");
+        isvy.afegeix(lloguerGeneral);
+        System.out.println(isvy.informe());
+    }
+    @Test
+    public void testInforme2GENERAL() throws ParseException {
+        Vehicle vehicleGeneral = new Vehicle("CL6", "Mercedez", Vehicle.GENERAL);
+        Vehicle vehicleGeneral2 = new Vehicle("CL6", "Mercedez", Vehicle.GENERAL);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("d/M/yyyy");
+        Date date = dateFormat.parse("2/8/2013");
+        Date date2 = dateFormat.parse("9/8/2013");
+        Lloguer lloguerGeneral = new Lloguer(date, 2, vehicleGeneral);
+        Lloguer lloguerGeneral2 = new Lloguer(date, 2, vehicleGeneral2);
+        Client isvy = new Client("Y1846344X", "Isvy Kelison", "665239406");
+        isvy.afegeix(lloguerGeneral);
+        isvy.afegeix(lloguerGeneral2);
+        System.out.println(isvy.informe());
+    }
+    @Test
+    public void testInforme1LUXE() throws ParseException {
+        Vehicle vehicleLuxe = new Vehicle("Enzo", "Ferrari", Vehicle.LUXE);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("d/M/yyyy");
+        Date date = dateFormat.parse("2/8/2013");
+        Lloguer lloguerLuxer = new Lloguer(date, 2, vehicleLuxe);
+        Client isvy = new Client("Y1846344X", "Isvy Kelison", "665239406");
+        isvy.afegeix(lloguerLuxer);
+        System.out.println(isvy.informe());
+    }
+    @Test
+    public void testInforme2LUXE() throws ParseException {
+        Vehicle vehicleLuxe = new Vehicle("Enzo", "Ferrari", Vehicle.LUXE);
+        Vehicle vehicleLuxe2 = new Vehicle("Enzo", "Ferrari", Vehicle.LUXE);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("d/M/yyyy");
+        Date date = dateFormat.parse("2/8/2013");
+        Date date2 = dateFormat.parse("9/8/2013");
+        Lloguer lloguerLuxe = new Lloguer(date, 2, vehicleLuxe);
+        Lloguer lloguerLuxe2 = new Lloguer(date, 2, vehicleLuxe2);
+        Client isvy = new Client("Y1846344X", "Isvy Kelison", "665239406");
+        isvy.afegeix(lloguerLuxe);
+        isvy.afegeix(lloguerLuxe2);
+        System.out.println(isvy.informe());
+    }
+
+    @Test
+    public void testInforme1diaLUXE() throws ParseException {
+        Vehicle vehicleLuxe = new Vehicle("Enzo", "Ferrari", Vehicle.LUXE);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("d/M/yyyy");
+        Date date = dateFormat.parse("2/8/2013");
+        Lloguer lloguerLuxer = new Lloguer(date, 1, vehicleLuxe);
+        Client isvy = new Client("Y1846344X", "Isvy Kelison", "665239406");
+        isvy.afegeix(lloguerLuxer);
+        System.out.println(isvy.informe());
+    }
+
+
 }
