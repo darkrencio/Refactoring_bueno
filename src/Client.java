32f1cde5bcd6b3a70a9b812373d3557847e18891
Isvy Araujo <ikelisonva.jmzafra@gmail.com>
2019-05-25 15:32:15 +0200

    Exercici 12. Extracció del càlcul de bonificacions: Completat

--- src/Client.java
+++ src/Client.java
@@ -50,30 +50,30 @@ public class Client {
                 getNom() +
                 " (" + getNif() + ")\n";
         for (Lloguer lloguer: lloguers) {
-            double quantitat = lloguer.quantitat();
-
-            // afegeix lloguers freqüents
-            bonificacions ++;
-
-            // afegeix bonificació per dos dies de lloguer de Luxe
-            if (lloguer.getVehicle().getCategoria() == Vehicle.LUXE &&
-                    lloguer.getDies()>1 ) {
-                bonificacions ++;
-            }
-
+            bonificacions += bonificacionsDeLloguer(lloguer);
             // composa els resultats d'aquest lloguer
             resultat += "\t" +
                     lloguer.getVehicle().getMarca() +
                     " " +
                     lloguer.getVehicle().getModel() + ": " +
-                    (quantitat * 30) + "€" + "\n";
-            total += quantitat * 30;
+                    (lloguer.quantitat() * 30) + "€" + "\n";
+            total += lloguer.quantitat() * 30;
         }
 
         // afegeix informació final
         resultat += "Import a pagar: " + total + "€\n" +
                 "Punts guanyats: " + bonificacions + "\n";
         return resultat;
+    }
+
+    public int bonificacionsDeLloguer(Lloguer lloguer){
+        double quantitat = lloguer.quantitat();
+
+        // afegeix bonificació per dos dies de lloguer de Luxe
+        if (lloguer.getVehicle().getCategoria() == Vehicle.LUXE && lloguer.getDies()>1 ) {
+            return 2;
+        }
+        return 1;
     }
 
     @Override
